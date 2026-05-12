46c6118310a9f798e373b9b1cb36e51e798c834e
emerss001 <emersonnevess80@gmail.com>
2026-05-12 10:49:03 -0300

    atualizando configuração do better auth

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -3,6 +3,9 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+    baseURL: process.env.API_URL || "http://localhost:3333",
+    trustedOrigins: ["http://localhost:3333", "http://localhost:3000"],
+    
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
