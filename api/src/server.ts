33847d66cf3f7ed4a4af72c8319efb2f5da24815
emerss001 <emersonnevess80@gmail.com>
2026-05-23 17:51:41 -0300

    adicionando log de controle de versão

--- api/src/server.ts
+++ api/src/server.ts
@@ -129,4 +129,5 @@ startMaestroCron();
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
     console.log("Server running on http://localhost:3333");
+    console.log("------ versão última do código ------");
 });
